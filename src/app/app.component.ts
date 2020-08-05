11686dcd5eea051fc59e53c5d21e1437bd125576
Rodrigo Rossi <rdrg.rossi@gmail.com>
2020-08-05 11:04:08 -0300

    adicionado cabeçalho padrão SeniorX

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AnalyticsHeaderService } from '@senior-gestao-pessoas/angular-components';
+import { finalize } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'seniorx-ui-starter';
+  pageLoading: boolean;
+  activeEmployees: any[];
+
+  constructor(
+    private readonly headerService: AnalyticsHeaderService
+  ) {
+
+  }
+
+  ngOnInit(): void {
+    // this.pageLoading = true;
+    // this.headerService.personActiveEmployeesSubject
+    //   .pipe(
+    //     finalize(() => {
+    //       this.pageLoading = false;
+    //     })
+    //   )
+    //   .subscribe((employees: any) => {
+    //     this.activeEmployees = employees;
+    //   });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,28 @@
+import { registerLocaleData } from '@angular/common';
+import ptBr from '@angular/common/locales/pt';
+
 import { BrowserModule } from '@angular/platform-browser';
 import { MessageService } from 'primeng/components/common/messageservice';
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import {
+  AnalyticsHeaderModule,
+  BreadcrumbModule,
+  ErrorPageModule,
+  GoogleAnalyticsModule,
+  RedirectPageModule
+} from '@senior-gestao-pessoas/angular-components';
+import { LoadingStateModule, LocaleModule } from '@seniorsistemas/angular-components';
+import { RestUrl, TranslationsModule } from '@seniorsistemas/platform-components';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
+
+import { default as fallback } from '../locale/pt-BR.json';
+import { TranslateModule } from '@ngx-translate/core';
+registerLocaleData(ptBr);
 
 @NgModule({
   declarations: [
@@ -16,9 +32,26 @@ import { HttpClientModule } from '@angular/common/http';
     BrowserModule,
     BrowserAnimationsModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    AnalyticsHeaderModule,
+    LoadingStateModule,
+
+    TranslateModule.forRoot(),
+    TranslationsModule.forRoot([
+      {
+        domain: 'hcm',
+        service: 'vacancymanagement',
+        fallback
+      },
+      {
+        domain: 'platform',
+        service: 'filter_service',
+        fallback
+      }
+    ]),
+
   ],
-  providers: [MessageService],
+  providers: [MessageService, { provide: LOCALE_ID, useValue: 'pt-BR' }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
